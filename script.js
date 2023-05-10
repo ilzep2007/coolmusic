cb6e9fecbc2aa6c06bc3d2827d5b9ceeacda6cde
ilzep2007 <114050636+ilzep2007@users.noreply.github.com>
2023-05-10 09:35:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const coolButton = document.querySelector('.js-cool');
+
+console.log(coolButton)("Clik",()=>
+[
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley')
+]
+ )
